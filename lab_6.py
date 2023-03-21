a281337c6fadb80b285a1bc32411c4b835ad51e0
sergio7773 <sergio7773@outlook.com>
2023-03-21 18:04:37 -0400

    created decode function and fixed logic errors

--- lab_6.py
+++ lab_6.py
@@ -2,9 +2,20 @@ def encoder(var):
     encode_password = ""
     for char in var:
         char = int(char)
-        char += 5
+        char += 3
         encode_password += str(char)
     return encode_password
+
+
+def decode(password):
+    decoded_password = ""
+    for i in password:
+        i = int(i)
+        i -= 3
+        decoded_password += str(i)
+    return decoded_password
+
+
 if __name__ == "__main__":
     while True:
         print("Menu")
@@ -12,12 +23,15 @@ if __name__ == "__main__":
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
-        choice = input("Please enter an option:")
+        print()
+        choice = int(input("Please enter an option: "))
         if choice == 1:
-            char = input("Please enter your password to encode:")
+            var = input("Please enter your password to encode: ")
             password = encoder(var)
-            print("Your password has been encoded and stored")
+            print("Your password has been encoded and stored!")
+            print()
         elif choice == 2:
-            pass
+            decoded_password = decode(password)
+            print(f"The encoded password is {password}, and the original password is {decoded_password}")
         elif choice == 3:
-            exit()
+            break
